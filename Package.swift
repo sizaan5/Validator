120d7931a934305fd92301c8cbcdaa9d2bf4f0be
Izaan Saleem <sizaan55@gmail.com>
2022-11-23 14:57:23 +0500

    version 0.0.2

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "Validator",
+    platforms: [
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -18,11 +21,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
+        .binaryTarget(
             name: "Validator",
-            dependencies: []),
-        .testTarget(
-            name: "ValidatorTests",
-            dependencies: ["Validator"]),
+            path: "./Sources/Validator.xcframework"
+        )
     ]
 )
