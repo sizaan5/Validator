678d5bc0e40c8985609550362866325b96a9b878
Izaan Saleem <sizaan55@gmail.com>
2022-11-23 11:03:04 +0500

    first commit

--- Sources/Validator/Validator.swift
+++ Sources/Validator/Validator.swift
@@ -1,6 +1,23 @@
-public struct Validator {
-    public private(set) var text = "Hello, World!"
+//
+//  AppHelper.swift
+//  RealtimeChat
+//
+//  Created by Izaan Saleem on 27/10/2022.
+//
 
-    public init() {
+import Foundation
+
+public class Validator {
+    public static func isValid(email: String) -> Bool {
+        let emailRegex = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,6}"
+        return NSPredicate(format: "SELF MATCHES %@", emailRegex).evaluate(with: email)
+    }
+    
+    public static func isValid(password: String) -> Bool {
+        return (password.count < 8) ? false : true
+    }
+    
+    public static func isValid(name: String) -> Bool {
+        return (name.count < 2 || name.count > 33) ? false : true
     }
 }
--- Tests/ValidatorTests/ValidatorTests.swift
+++ Tests/ValidatorTests/ValidatorTests.swift
@@ -6,6 +6,6 @@ final class ValidatorTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Validator().text, "Hello, World!")
+        //XCTAssertEqual(Validator().text, "Hello, World!")
     }
 }
